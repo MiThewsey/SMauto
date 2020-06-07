07f6308e52200974ba662b1037e7bde99a1f30fc
Matthew Thewsey <mithewsey@gmail.com>
2020-06-07 14:58:30 +0100

    Single and Slideshow added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SMauto
 {
-    partial class Form1
+    partial class F_SMauto
     {
         /// <summary>
         /// Required designer variable.
@@ -31,14 +31,38 @@
             this.B_Clear = new System.Windows.Forms.Button();
             this.B_FileCheck = new System.Windows.Forms.Button();
             this.L_SMauto = new System.Windows.Forms.Label();
-            this.TB_Tweet1 = new System.Windows.Forms.TextBox();
+            this.TB_tweet1 = new System.Windows.Forms.TextBox();
             this.L_Tweet1 = new System.Windows.Forms.Label();
             this.B_Update = new System.Windows.Forms.Button();
+            this.CB_tweet1 = new System.Windows.Forms.CheckBox();
+            this.CB_tweet2 = new System.Windows.Forms.CheckBox();
+            this.L_tweet2 = new System.Windows.Forms.Label();
+            this.TB_tweet2 = new System.Windows.Forms.TextBox();
+            this.CB_tweet3 = new System.Windows.Forms.CheckBox();
+            this.L_tweet3 = new System.Windows.Forms.Label();
+            this.TB_tweet3 = new System.Windows.Forms.TextBox();
+            this.CB_tweet4 = new System.Windows.Forms.CheckBox();
+            this.L_tweet4 = new System.Windows.Forms.Label();
+            this.TB_tweet4 = new System.Windows.Forms.TextBox();
+            this.CB_tweet5 = new System.Windows.Forms.CheckBox();
+            this.L_tweet5 = new System.Windows.Forms.Label();
+            this.TB_tweet5 = new System.Windows.Forms.TextBox();
+            this.RB_tweet1 = new System.Windows.Forms.RadioButton();
+            this.RB_tweet2 = new System.Windows.Forms.RadioButton();
+            this.RB_tweet3 = new System.Windows.Forms.RadioButton();
+            this.RB_tweet4 = new System.Windows.Forms.RadioButton();
+            this.RB_tweet5 = new System.Windows.Forms.RadioButton();
+            this.RB_single = new System.Windows.Forms.RadioButton();
+            this.RB_slideshow = new System.Windows.Forms.RadioButton();
+            this.GB_SingleOrSlideshow = new System.Windows.Forms.GroupBox();
+            this.GB_ActiveArea = new System.Windows.Forms.GroupBox();
+            this.GB_SingleOrSlideshow.SuspendLayout();
+            this.GB_ActiveArea.SuspendLayout();
             this.SuspendLayout();
             // 
             // B_Clear
             // 
-            this.B_Clear.Location = new System.Drawing.Point(103, 129);
+            this.B_Clear.Location = new System.Drawing.Point(23, 27);
             this.B_Clear.Name = "B_Clear";
             this.B_Clear.Size = new System.Drawing.Size(75, 23);
             this.B_Clear.TabIndex = 1;
@@ -48,7 +72,7 @@
             // 
             // B_FileCheck
             // 
-            this.B_FileCheck.Location = new System.Drawing.Point(103, 100);
+            this.B_FileCheck.Location = new System.Drawing.Point(17, 37);
             this.B_FileCheck.Name = "B_FileCheck";
             this.B_FileCheck.Size = new System.Drawing.Size(75, 23);
             this.B_FileCheck.TabIndex = 2;
@@ -66,17 +90,17 @@
             this.L_SMauto.TabIndex = 3;
             this.L_SMauto.Text = "SMauto";
             // 
-            // TB_Tweet1
+            // TB_tweet1
             // 
-            this.TB_Tweet1.Location = new System.Drawing.Point(306, 100);
-            this.TB_Tweet1.Name = "TB_Tweet1";
-            this.TB_Tweet1.Size = new System.Drawing.Size(100, 20);
-            this.TB_Tweet1.TabIndex = 4;
+            this.TB_tweet1.Location = new System.Drawing.Point(214, 26);
+            this.TB_tweet1.Name = "TB_tweet1";
+            this.TB_tweet1.Size = new System.Drawing.Size(100, 20);
+            this.TB_tweet1.TabIndex = 4;
             // 
             // L_Tweet1
             // 
             this.L_Tweet1.AutoSize = true;
-            this.L_Tweet1.Location = new System.Drawing.Point(257, 103);
+            this.L_Tweet1.Location = new System.Drawing.Point(165, 29);
             this.L_Tweet1.Name = "L_Tweet1";
             this.L_Tweet1.Size = new System.Drawing.Size(43, 13);
             this.L_Tweet1.TabIndex = 5;
@@ -85,7 +109,7 @@
             // 
             // B_Update
             // 
-            this.B_Update.Location = new System.Drawing.Point(103, 158);
+            this.B_Update.Location = new System.Drawing.Point(23, 56);
             this.B_Update.Name = "B_Update";
             this.B_Update.Size = new System.Drawing.Size(75, 23);
             this.B_Update.TabIndex = 6;
@@ -93,19 +117,247 @@
             this.B_Update.UseVisualStyleBackColor = true;
             this.B_Update.Click += new System.EventHandler(this.B_Update_Click);
             // 
-            // Form1
+            // CB_tweet1
+            // 
+            this.CB_tweet1.AutoSize = true;
+            this.CB_tweet1.Location = new System.Drawing.Point(320, 29);
+            this.CB_tweet1.Name = "CB_tweet1";
+            this.CB_tweet1.Size = new System.Drawing.Size(15, 14);
+            this.CB_tweet1.TabIndex = 7;
+            this.CB_tweet1.UseVisualStyleBackColor = true;
+            // 
+            // CB_tweet2
+            // 
+            this.CB_tweet2.AutoSize = true;
+            this.CB_tweet2.Location = new System.Drawing.Point(320, 55);
+            this.CB_tweet2.Name = "CB_tweet2";
+            this.CB_tweet2.Size = new System.Drawing.Size(15, 14);
+            this.CB_tweet2.TabIndex = 10;
+            this.CB_tweet2.UseVisualStyleBackColor = true;
+            // 
+            // L_tweet2
+            // 
+            this.L_tweet2.AutoSize = true;
+            this.L_tweet2.Location = new System.Drawing.Point(165, 55);
+            this.L_tweet2.Name = "L_tweet2";
+            this.L_tweet2.Size = new System.Drawing.Size(43, 13);
+            this.L_tweet2.TabIndex = 9;
+            this.L_tweet2.Text = "Tweet2";
+            this.L_tweet2.Click += new System.EventHandler(this.L_tweet2_Click);
+            // 
+            // TB_tweet2
+            // 
+            this.TB_tweet2.Location = new System.Drawing.Point(214, 52);
+            this.TB_tweet2.Name = "TB_tweet2";
+            this.TB_tweet2.Size = new System.Drawing.Size(100, 20);
+            this.TB_tweet2.TabIndex = 8;
+            // 
+            // CB_tweet3
+            // 
+            this.CB_tweet3.AutoSize = true;
+            this.CB_tweet3.Location = new System.Drawing.Point(320, 81);
+            this.CB_tweet3.Name = "CB_tweet3";
+            this.CB_tweet3.Size = new System.Drawing.Size(15, 14);
+            this.CB_tweet3.TabIndex = 13;
+            this.CB_tweet3.UseVisualStyleBackColor = true;
+            // 
+            // L_tweet3
+            // 
+            this.L_tweet3.AutoSize = true;
+            this.L_tweet3.Location = new System.Drawing.Point(165, 81);
+            this.L_tweet3.Name = "L_tweet3";
+            this.L_tweet3.Size = new System.Drawing.Size(43, 13);
+            this.L_tweet3.TabIndex = 12;
+            this.L_tweet3.Text = "Tweet3";
+            this.L_tweet3.Click += new System.EventHandler(this.L_tweet3_Click);
+            // 
+            // TB_tweet3
+            // 
+            this.TB_tweet3.Location = new System.Drawing.Point(214, 78);
+            this.TB_tweet3.Name = "TB_tweet3";
+            this.TB_tweet3.Size = new System.Drawing.Size(100, 20);
+            this.TB_tweet3.TabIndex = 11;
+            // 
+            // CB_tweet4
+            // 
+            this.CB_tweet4.AutoSize = true;
+            this.CB_tweet4.Location = new System.Drawing.Point(320, 107);
+            this.CB_tweet4.Name = "CB_tweet4";
+            this.CB_tweet4.Size = new System.Drawing.Size(15, 14);
+            this.CB_tweet4.TabIndex = 16;
+            this.CB_tweet4.UseVisualStyleBackColor = true;
+            // 
+            // L_tweet4
+            // 
+            this.L_tweet4.AutoSize = true;
+            this.L_tweet4.Location = new System.Drawing.Point(165, 107);
+            this.L_tweet4.Name = "L_tweet4";
+            this.L_tweet4.Size = new System.Drawing.Size(43, 13);
+            this.L_tweet4.TabIndex = 15;
+            this.L_tweet4.Text = "Tweet4";
+            this.L_tweet4.Click += new System.EventHandler(this.L_tweet4_Click);
+            // 
+            // TB_tweet4
+            // 
+            this.TB_tweet4.Location = new System.Drawing.Point(214, 104);
+            this.TB_tweet4.Name = "TB_tweet4";
+            this.TB_tweet4.Size = new System.Drawing.Size(100, 20);
+            this.TB_tweet4.TabIndex = 14;
+            // 
+            // CB_tweet5
+            // 
+            this.CB_tweet5.AutoSize = true;
+            this.CB_tweet5.Location = new System.Drawing.Point(320, 133);
+            this.CB_tweet5.Name = "CB_tweet5";
+            this.CB_tweet5.Size = new System.Drawing.Size(15, 14);
+            this.CB_tweet5.TabIndex = 19;
+            this.CB_tweet5.UseVisualStyleBackColor = true;
+            // 
+            // L_tweet5
+            // 
+            this.L_tweet5.AutoSize = true;
+            this.L_tweet5.Location = new System.Drawing.Point(165, 133);
+            this.L_tweet5.Name = "L_tweet5";
+            this.L_tweet5.Size = new System.Drawing.Size(43, 13);
+            this.L_tweet5.TabIndex = 18;
+            this.L_tweet5.Text = "Tweet5";
+            this.L_tweet5.Click += new System.EventHandler(this.L_tweet5_Click);
+            // 
+            // TB_tweet5
+            // 
+            this.TB_tweet5.Location = new System.Drawing.Point(214, 130);
+            this.TB_tweet5.Name = "TB_tweet5";
+            this.TB_tweet5.Size = new System.Drawing.Size(100, 20);
+            this.TB_tweet5.TabIndex = 17;
+            // 
+            // RB_tweet1
+            // 
+            this.RB_tweet1.AutoSize = true;
+            this.RB_tweet1.Location = new System.Drawing.Point(340, 28);
+            this.RB_tweet1.Name = "RB_tweet1";
+            this.RB_tweet1.Size = new System.Drawing.Size(14, 13);
+            this.RB_tweet1.TabIndex = 20;
+            this.RB_tweet1.TabStop = true;
+            this.RB_tweet1.UseVisualStyleBackColor = true;
+            // 
+            // RB_tweet2
+            // 
+            this.RB_tweet2.AutoSize = true;
+            this.RB_tweet2.Location = new System.Drawing.Point(340, 55);
+            this.RB_tweet2.Name = "RB_tweet2";
+            this.RB_tweet2.Size = new System.Drawing.Size(14, 13);
+            this.RB_tweet2.TabIndex = 21;
+            this.RB_tweet2.TabStop = true;
+            this.RB_tweet2.UseVisualStyleBackColor = true;
+            // 
+            // RB_tweet3
+            // 
+            this.RB_tweet3.AutoSize = true;
+            this.RB_tweet3.Location = new System.Drawing.Point(340, 81);
+            this.RB_tweet3.Name = "RB_tweet3";
+            this.RB_tweet3.Size = new System.Drawing.Size(14, 13);
+            this.RB_tweet3.TabIndex = 22;
+            this.RB_tweet3.TabStop = true;
+            this.RB_tweet3.UseVisualStyleBackColor = true;
+            // 
+            // RB_tweet4
+            // 
+            this.RB_tweet4.AutoSize = true;
+            this.RB_tweet4.Location = new System.Drawing.Point(340, 107);
+            this.RB_tweet4.Name = "RB_tweet4";
+            this.RB_tweet4.Size = new System.Drawing.Size(14, 13);
+            this.RB_tweet4.TabIndex = 23;
+            this.RB_tweet4.TabStop = true;
+            this.RB_tweet4.UseVisualStyleBackColor = true;
+            // 
+            // RB_tweet5
+            // 
+            this.RB_tweet5.AutoSize = true;
+            this.RB_tweet5.Location = new System.Drawing.Point(340, 133);
+            this.RB_tweet5.Name = "RB_tweet5";
+            this.RB_tweet5.Size = new System.Drawing.Size(14, 13);
+            this.RB_tweet5.TabIndex = 24;
+            this.RB_tweet5.TabStop = true;
+            this.RB_tweet5.UseVisualStyleBackColor = true;
+            // 
+            // RB_single
+            // 
+            this.RB_single.AutoSize = true;
+            this.RB_single.Location = new System.Drawing.Point(18, 23);
+            this.RB_single.Name = "RB_single";
+            this.RB_single.Size = new System.Drawing.Size(54, 17);
+            this.RB_single.TabIndex = 25;
+            this.RB_single.TabStop = true;
+            this.RB_single.Text = "Single";
+            this.RB_single.UseVisualStyleBackColor = true;
+            // 
+            // RB_slideshow
+            // 
+            this.RB_slideshow.AutoSize = true;
+            this.RB_slideshow.Location = new System.Drawing.Point(18, 46);
+            this.RB_slideshow.Name = "RB_slideshow";
+            this.RB_slideshow.Size = new System.Drawing.Size(73, 17);
+            this.RB_slideshow.TabIndex = 26;
+            this.RB_slideshow.TabStop = true;
+            this.RB_slideshow.Text = "Slideshow";
+            this.RB_slideshow.UseVisualStyleBackColor = true;
+            // 
+            // GB_SingleOrSlideshow
+            // 
+            this.GB_SingleOrSlideshow.Controls.Add(this.RB_slideshow);
+            this.GB_SingleOrSlideshow.Controls.Add(this.RB_single);
+            this.GB_SingleOrSlideshow.Location = new System.Drawing.Point(23, 107);
+            this.GB_SingleOrSlideshow.Name = "GB_SingleOrSlideshow";
+            this.GB_SingleOrSlideshow.Size = new System.Drawing.Size(105, 82);
+            this.GB_SingleOrSlideshow.TabIndex = 27;
+            this.GB_SingleOrSlideshow.TabStop = false;
+            // 
+            // GB_ActiveArea
+            // 
+            this.GB_ActiveArea.Controls.Add(this.B_Update);
+            this.GB_ActiveArea.Controls.Add(this.GB_SingleOrSlideshow);
+            this.GB_ActiveArea.Controls.Add(this.B_Clear);
+            this.GB_ActiveArea.Controls.Add(this.RB_tweet5);
+            this.GB_ActiveArea.Controls.Add(this.TB_tweet1);
+            this.GB_ActiveArea.Controls.Add(this.RB_tweet4);
+            this.GB_ActiveArea.Controls.Add(this.L_Tweet1);
+            this.GB_ActiveArea.Controls.Add(this.RB_tweet3);
+            this.GB_ActiveArea.Controls.Add(this.CB_tweet1);
+            this.GB_ActiveArea.Controls.Add(this.RB_tweet2);
+            this.GB_ActiveArea.Controls.Add(this.TB_tweet2);
+            this.GB_ActiveArea.Controls.Add(this.RB_tweet1);
+            this.GB_ActiveArea.Controls.Add(this.L_tweet2);
+            this.GB_ActiveArea.Controls.Add(this.CB_tweet5);
+            this.GB_ActiveArea.Controls.Add(this.CB_tweet2);
+            this.GB_ActiveArea.Controls.Add(this.L_tweet5);
+            this.GB_ActiveArea.Controls.Add(this.TB_tweet3);
+            this.GB_ActiveArea.Controls.Add(this.TB_tweet5);
+            this.GB_ActiveArea.Controls.Add(this.L_tweet3);
+            this.GB_ActiveArea.Controls.Add(this.CB_tweet4);
+            this.GB_ActiveArea.Controls.Add(this.CB_tweet3);
+            this.GB_ActiveArea.Controls.Add(this.L_tweet4);
+            this.GB_ActiveArea.Controls.Add(this.TB_tweet4);
+            this.GB_ActiveArea.Location = new System.Drawing.Point(133, 37);
+            this.GB_ActiveArea.Name = "GB_ActiveArea";
+            this.GB_ActiveArea.Size = new System.Drawing.Size(458, 235);
+            this.GB_ActiveArea.TabIndex = 28;
+            this.GB_ActiveArea.TabStop = false;
+            // 
+            // F_SMauto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.B_Update);
-            this.Controls.Add(this.L_Tweet1);
-            this.Controls.Add(this.TB_Tweet1);
+            this.Controls.Add(this.GB_ActiveArea);
             this.Controls.Add(this.L_SMauto);
             this.Controls.Add(this.B_FileCheck);
-            this.Controls.Add(this.B_Clear);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "F_SMauto";
+            this.Text = "SMauto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GB_SingleOrSlideshow.ResumeLayout(false);
+            this.GB_SingleOrSlideshow.PerformLayout();
+            this.GB_ActiveArea.ResumeLayout(false);
+            this.GB_ActiveArea.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,9 +367,31 @@
         private System.Windows.Forms.Button B_Clear;
         private System.Windows.Forms.Button B_FileCheck;
         private System.Windows.Forms.Label L_SMauto;
-        private System.Windows.Forms.TextBox TB_Tweet1;
+        private System.Windows.Forms.TextBox TB_tweet1;
         private System.Windows.Forms.Label L_Tweet1;
         private System.Windows.Forms.Button B_Update;
+        private System.Windows.Forms.CheckBox CB_tweet1;
+        private System.Windows.Forms.CheckBox CB_tweet2;
+        private System.Windows.Forms.Label L_tweet2;
+        private System.Windows.Forms.TextBox TB_tweet2;
+        private System.Windows.Forms.CheckBox CB_tweet3;
+        private System.Windows.Forms.Label L_tweet3;
+        private System.Windows.Forms.TextBox TB_tweet3;
+        private System.Windows.Forms.CheckBox CB_tweet4;
+        private System.Windows.Forms.Label L_tweet4;
+        private System.Windows.Forms.TextBox TB_tweet4;
+        private System.Windows.Forms.CheckBox CB_tweet5;
+        private System.Windows.Forms.Label L_tweet5;
+        private System.Windows.Forms.TextBox TB_tweet5;
+        private System.Windows.Forms.RadioButton RB_tweet1;
+        private System.Windows.Forms.RadioButton RB_tweet2;
+        private System.Windows.Forms.RadioButton RB_tweet3;
+        private System.Windows.Forms.RadioButton RB_tweet4;
+        private System.Windows.Forms.RadioButton RB_tweet5;
+        private System.Windows.Forms.RadioButton RB_single;
+        private System.Windows.Forms.RadioButton RB_slideshow;
+        private System.Windows.Forms.GroupBox GB_SingleOrSlideshow;
+        private System.Windows.Forms.GroupBox GB_ActiveArea;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,52 +9,170 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+
 namespace SMauto
 {
-    public partial class Form1 : Form
+
+    public partial class F_SMauto : Form
     {
-        public Form1()
+
+        #region FL = File Locations
+        readonly string FL_origional    = @"Dependencies\\origional.html";
+        readonly string FL_cssstyle     = @"Dependencies\\cssstyles.css";
+        readonly string FL_jquery       = @"Dependencies\\jquery-1.4.2.min.js";
+        readonly string FL_OBS_Twitter  = @"Dependencies\\OBS_Twitter.html";
+        #endregion
+
+        public F_SMauto()
         {
             InitializeComponent();
+            GB_ActiveArea.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
 
         private void B_File_Click(object sender, EventArgs e)
         {
             bool file1, file2, file3;
-            file1 = System.IO.File.Exists("Dependencies\\origional.html");
-            file2 = System.IO.File.Exists("Dependencies\\cssstyles.css");
-            file3 = System.IO.File.Exists("Dependencies\\jquery-1.4.2.min.js");
+            file1 = System.IO.File.Exists(FL_origional);
+            file2 = System.IO.File.Exists(FL_cssstyle);
+            file3 = System.IO.File.Exists(FL_jquery);
 
             if (file1 & file2 & file3) 
             {
                 B_FileCheck.BackColor = Color.Green;
+                GB_ActiveArea.Enabled = true;
             }
             else
             {
                 B_FileCheck.BackColor = Color.Red;
+                GB_ActiveArea.Enabled = false;
             }
 
         }
 
         private void B_Clear_Click(object sender, EventArgs e)
         {
-            File.Delete(@"Dependencies\\OBStwitter.html");
-            File.Copy(@"Dependencies\\origional.html", @"Dependencies\\OBStwitter.html");
+            File.Delete(FL_OBS_Twitter);
+            File.Copy(FL_origional, FL_OBS_Twitter);
         }
 
-        private void L_Tweet1_Click(object sender, EventArgs e)
-        {
-            TB_Tweet1.Clear();
-        }
+
 
         private void B_Update_Click(object sender, EventArgs e)
         {
-            var alllines = File.ReadAllLines(@"Dependencies\\OBStwitter.html").ToList();
-            alllines.Insert(14, "");
-            alllines.Insert(15, "<li>");
-            alllines.Insert(16, TB_Tweet1.Text);
-            alllines.Insert(17, "</li>");
-            File.WriteAllLines(@"Dependencies\\OBStwitter.html", alllines.ToArray());
+            if (RB_single.Checked)
+            {
+                if (RB_tweet1.Checked)
+                {
+                    File.Delete(FL_OBS_Twitter);
+                    File.WriteAllText(FL_OBS_Twitter, TB_tweet1.Text);
+                }
+                if (RB_tweet2.Checked)
+                {
+                    File.Delete(FL_OBS_Twitter);
+                    File.WriteAllText(FL_OBS_Twitter, TB_tweet2.Text);
+                }
+                if (RB_tweet3.Checked)
+                {
+                    File.Delete(FL_OBS_Twitter);
+                    File.WriteAllText(FL_OBS_Twitter, TB_tweet3.Text);
+                }
+                if (RB_tweet4.Checked)
+                {
+                    File.Delete(FL_OBS_Twitter);
+                    File.WriteAllText(FL_OBS_Twitter, TB_tweet4.Text);
+                }
+                if (RB_tweet5.Checked)
+                {
+                    File.Delete(FL_OBS_Twitter);
+                    File.WriteAllText(FL_OBS_Twitter, TB_tweet5.Text);
+                }
+            }
+            if (RB_slideshow.Checked)
+            {
+                File.Delete(FL_OBS_Twitter);
+                File.Copy(FL_origional, FL_OBS_Twitter);
+
+                //reverse order so that first tweet shows first
+                if (CB_tweet5.Checked)
+                {
+                    var alllines = File.ReadAllLines(FL_OBS_Twitter).ToList();
+                    alllines.Insert(14, "");
+                    alllines.Insert(15, "<li>");
+                    alllines.Insert(16, TB_tweet5.Text);
+                    alllines.Insert(17, "</li>");
+                    File.WriteAllLines(FL_OBS_Twitter, alllines.ToArray());
+                }
+                if (CB_tweet4.Checked)
+                {
+                    var alllines = File.ReadAllLines(FL_OBS_Twitter).ToList();
+                    alllines.Insert(14, "");
+                    alllines.Insert(15, "<li>");
+                    alllines.Insert(16, TB_tweet4.Text);
+                    alllines.Insert(17, "</li>");
+                    File.WriteAllLines(FL_OBS_Twitter, alllines.ToArray());
+                }
+                if (CB_tweet3.Checked)
+                {
+                    var alllines = File.ReadAllLines(FL_OBS_Twitter).ToList();
+                    alllines.Insert(14, "");
+                    alllines.Insert(15, "<li>");
+                    alllines.Insert(16, TB_tweet3.Text);
+                    alllines.Insert(17, "</li>");
+                    File.WriteAllLines(FL_OBS_Twitter, alllines.ToArray());
+                }
+                if (CB_tweet2.Checked)
+                {
+                    var alllines = File.ReadAllLines(FL_OBS_Twitter).ToList();
+                    alllines.Insert(14, "");
+                    alllines.Insert(15, "<li>");
+                    alllines.Insert(16, TB_tweet2.Text);
+                    alllines.Insert(17, "</li>");
+                    File.WriteAllLines(FL_OBS_Twitter, alllines.ToArray());
+                }
+                if (CB_tweet1.Checked)
+                {
+                    var alllines = File.ReadAllLines(FL_OBS_Twitter).ToList();
+                    alllines.Insert(14, "");
+                    alllines.Insert(15, "<li>");
+                    alllines.Insert(16, TB_tweet1.Text);
+                    alllines.Insert(17, "</li>");
+                    File.WriteAllLines(FL_OBS_Twitter, alllines.ToArray());
+                }
+            }
+
         }
+
+        #region Clear textbox buttons
+        private void L_Tweet1_Click(object sender, EventArgs e)
+        {
+            TB_tweet1.Clear();
+        }
+
+        private void L_tweet2_Click(object sender, EventArgs e)
+        {
+            TB_tweet2.Clear();
+        }
+
+        private void L_tweet3_Click(object sender, EventArgs e)
+        {
+            TB_tweet3.Clear();
+        }
+
+        private void L_tweet4_Click(object sender, EventArgs e)
+        {
+            TB_tweet4.Clear();
+        }
+
+        private void L_tweet5_Click(object sender, EventArgs e)
+        {
+            TB_tweet5.Clear();
+        }
+        #endregion
     }
 }
